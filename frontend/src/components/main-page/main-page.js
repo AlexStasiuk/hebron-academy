0574e2578269fbcbeca3285e760ab850575e438a
AlexStasiuk <sov853972@gmail.com>
2021-09-10 21:06:38 +0300

    add second block

--- frontend/src/components/main-page/main-page.js
+++ frontend/src/components/main-page/main-page.js
@@ -1,6 +1,11 @@
 import React from 'react';
 
 import './main-page.css';
+import {ReactComponent as MissionLogo} from './Mission.svg';
+import {ReactComponent as VisionLogo} from './vision.svg';
+import {ReactComponent as ContactsLogo} from './contacts.svg';
+import {ReactComponent as CredentialsLogo} from './credentials.svg';
+
 import HebronHeaderImage from './Hebron-Header.png';
 
 const MainPage = () =>{
@@ -24,10 +29,24 @@ const MainPage = () =>{
                 </div>
                 <div className='col-10 third-block'>
                     <div className='links'>
-                        <span>LinkedIn</span>
-                        <span>Instagram</span>
-                        <span>Facebook</span>
-                        <span>Youtube</span>
+                        
+                        <div className='link'>
+                            <div className='link-square'></div>
+                            <div>LinkedIn</div>
+                        </div>
+                        <div className='link'>
+                            <div className='link-square'></div>
+                            <div>Instagram</div>
+                        </div>
+                        <div className='link'>
+                            <div className='link-square'></div>
+                            <div>Facebook</div>
+                        </div>
+                        <div className='link'>
+                            <div className='link-square'></div>
+                            <div>Youtube</div>
+                        </div>
+
                     </div>
                     <div className='some-text'>
                         <span>Український центр</span>
@@ -44,8 +63,78 @@ const MainPage = () =>{
                     </div>
                 </div>
             </div>
+            <div className='empty-block-1 row no-gutters'>
+                <div className='col-12'></div>
+            </div>
+
             <div className='second-block-wrapper row no-gutters'>
-                f
+                <div className='col-1'></div>
+                <div className='col-10 main-header-block-wrapper'>
+                    <div className='main-header-block'>
+                        <div className='number-and-title'>
+                            <div className='header-number-wrapper'>
+                                <div className='number'>01</div>
+                                <div className='header-rectangle'></div>
+                            </div>
+                            <div className='header-title'>МІСІЯ</div>
+                        </div>
+                            
+                        <div className='header-logo'>
+                            <MissionLogo className='header-logo-svg'/>
+
+                        </div>
+                        <div className='header-text'>788888588</div>
+                    </div>
+
+
+                    <div className='main-header-block'>
+                        <div className='number-and-title'>
+                            <div className='header-number-wrapper'>
+                                <div className='number'>02</div>
+                                <div className='header-rectangle'></div>
+                            </div>
+                            <div className='header-title'>БАЧЕННЯ</div>
+                        </div>
+                            
+                        <div className='header-logo'>
+                            <VisionLogo className='header-logo-svg'/>
+
+                        </div>
+                        <div className='header-text'>Бути центром інноваційної освіти для сиріт та молоді з групи ризику, побудованої на християнських принципах.</div>
+                    </div>
+                    <div className='main-header-block'>
+                        <div className='number-and-title'>
+                            <div className='header-number-wrapper'>
+                                <div className='number'>03</div>
+                                <div className='header-rectangle'></div>
+                            </div>
+                            <div className='header-title'>РЕКВІЗИТИ</div>
+                        </div>
+                            
+                        <div className='header-logo'>
+                            <CredentialsLogo className='header-logo-svg'/>
+
+                        </div>
+                        <div className='header-text'>788888588</div>
+                    </div>
+                    <div className='main-header-block'>
+                        <div className='number-and-title'>
+                            <div className='header-number-wrapper'>
+                                <div className='number'>04</div>
+                                <div className='header-rectangle'></div>
+                            </div>
+                            <div className='header-title'>КОНТАКТИ</div>
+                        </div>
+                            
+                        <div className='header-logo'>
+                            <ContactsLogo className='header-logo-svg'/>
+
+                        </div>
+                        <div className='header-text'>788888588</div>
+                    </div>
+
+                </div>
+                <div className='col-1'></div>
             </div>
         </div>
     );
