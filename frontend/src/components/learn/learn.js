a163b53dce9ddbf75a40b70962a2e8e67ae3a801
AlexStasiuk <sov853972@gmail.com>
2021-10-02 15:12:47 +0300

    add hebron-needs-join-page into hebron needs

--- frontend/src/components/learn/learn.js
+++ frontend/src/components/learn/learn.js
@@ -18,58 +18,58 @@ import Contacts  from '../contacts';
 
 const Learn = (props) => {
     return(
-        <div className='content row no-gutters'>
-            <div className='learn'>
-                    <div className='col-1'></div>
-                    <div className='col-3 txt'>
-                        <div className='lr'>
-                            <img src={Stydy} alt=''/>
-                        </div>
-                        <div className="text-about">
-                            <div className='text-title'>
-                                <h1>ХЕВРОН</h1>
-                                <h1>IT Академiя</h1>
-                            </div>
-                            <ul className="about-study">
-                                <li><span>24 мiсяця навчання</span></li>
-                                <li><span>Студенти зі всієї України</span></li>
-                                <li><span>Неформальна освіта для молоді з соціально кризової категорії</span></li>
-                                <li><span>Індивідуальна система навчання та підходу</span></li>
-                                <li><span>Практика в ІТ компаніях</span></li>
-                                <li><span>Духовний, інтелектуальний та фізичний розвиток</span></li>
-                                <li><span>Забезпечення студентів житлом, харчуванням та ліками</span></li>
-                                <li><span>Працевлаштування випускників</span></li>
-                            </ul>
-                        </div>
+        <div className='content-learn no-gutters'>
+            <div className='learn-learn row no-gutters'>
+                <div className='col-1'></div>
+                <div className='col-3 txt-learn'>
+                    <div className='lr'>
+                        <img src={Stydy} alt=''/>
                     </div>
-                    <div className='books col-7'>
-                        <img src={Books} alt=''/>
+                    <div className="text-about-learn">
+                        <div className='text-title-learn'>
+                            <h1>ХЕВРОН</h1>
+                            <h1>IT Академiя</h1>
+                        </div>
+                        <ul className="about-study-learn">
+                            <li><span>24 мiсяця навчання</span></li>
+                            <li><span>Студенти зі всієї України</span></li>
+                            <li><span>Неформальна освіта для молоді з соціально кризової категорії</span></li>
+                            <li><span>Індивідуальна система навчання та підходу</span></li>
+                            <li><span>Практика в ІТ компаніях</span></li>
+                            <li><span>Духовний, інтелектуальний та фізичний розвиток</span></li>
+                            <li><span>Забезпечення студентів житлом, харчуванням та ліками</span></li>
+                            <li><span>Працевлаштування випускників</span></li>
+                        </ul>
                     </div>
+                </div>
+                <div className='books-learn col-8'>
+                    <img src={Books} alt=''/>
+                </div>
             </div>
-            <div className='work-academy row no-gutters'>
-                <div className='h'>
+            <div className='work-academy-learn row no-gutters'>
+                <div className='h-learn'>
                     <img src={Hebron} alt=''/>
                 </div>
                 
-                <div className='how-academy-works-wrapper '>
-                    <div className='dd'>
-                        <div className='d-a'></div>
-                        <div className='d-b'></div>
+                <div className='how-academy-works-wrapper-learn '>
+                    <div className='dd-learn'>
+                        <div className='d-a-learn'></div>
+                        <div className='d-b-learn'></div>
                     </div>
-                    <div className='head'>
+                    <div className='head-learn'>
                         <h1>Як Академія Працює?</h1>
                     </div>
                 </div>
                 
-                <div className='academy'>
+                <div className='academy-learn'>
                     
-                    <div className='a'>
-                        <div className='a-img'>
-                            <div className='one'>1</div> 
+                    <div className='a-learn'>
+                        <div className='a-img-learn'>
+                            <div className='one-learn'>1</div> 
                             <span>ВСТУП</span> 
                             <img src={Vector} alt=''/>
                         </div>
-                        <div className='a-p'>
+                        <div className='a-p-learn'>
                             <span>Молоді люди групи ризику, які завершили</span>
                             <span>навчання в школі та досягли повноліття,</span>
                             <span>можуть вступити в Академію.</span>
@@ -80,13 +80,13 @@ const Learn = (props) => {
                             <span>всього року.</span>
                         </div>
                     </div>
-                    <div className='a'>
-                        <div className='a-img'>
-                            <div className='one'>2</div>
+                    <div className='a-learn'>
+                        <div className='a-img-learn'>
+                            <div className='one-learn'>2</div>
                             <span>НАВЧАННЯ</span>
                             <img src={Vector2} alt=''/>
                         </div>
-                        <div className='a-p'>
+                        <div className='a-p-learn'>
                         
                             <span>Студент навчається за новітньою</span>
                             <span>24-місячною програмою,</span>
@@ -97,13 +97,13 @@ const Learn = (props) => {
                         
                         </div>
                     </div>
-                    <div className='a'>
-                        <div className='a-img'>
-                            <div className='one'>3</div>
+                    <div className='a-learn'>
+                        <div className='a-img-learn'>
+                            <div className='one-learn'>3</div>
                             <span>РОБОТА</span>
                             <img src={Vector3} alt=''/>
                         </div>
-                        <div className='a-p'>
+                        <div className='a-p-learn'>
                             <span>Після завершення навчання,</span>
                             <span>студент отримує Сертифікат,</span>
                             <span>який підтверджує компетентність</span>
@@ -116,30 +116,30 @@ const Learn = (props) => {
             </div>
 
             
-            <div className='hh row no-gutters'>
-                <div className='h-a col-4'>
+            <div className='hh-learn row no-gutters'>
+                <div className='h-a-learn col-4'>
                     <span>Тривекторна</span>
                     <span>програма навчання</span>
                     <span>Хеврон ІТ Академії</span>
                 </div>
 
-                <div className='h-b col-8'>
+                <div className='h-b-learn col-8'>
                     <span>Дана програма включає в себе <b>духовну, інтелектуальну</b> та <b>фізичну</b> складову</span>
                     <span>розвитку, а також дає змогу молоді виробити нову систему поведінки</span>
                     <span>у складних життєвих обставинах.</span>
                 </div>
             
             </div>
-                <div className='devel col-12'>
-                    <div className='d-a-t col-3'>
-                        <div className='img-a'>
+                <div className='devel-learn col-12'>
+                    <div className='d-a-t-learn col-3'>
+                        <div className='img-a-learn'>
                             <span>
                                 <h4>ІНТЕЛЕКТУАЛЬНИЙ</h4>
                                 <h4>РОЗВИТОК:</h4>
                                 <img src={Shape} alt=''/>
                             </span>
                         </div>
-                        <div className='d-a-p col-9'>
+                        <div className='d-a-p-learn col-9'>
                             <p>- Основи програмування</p>
                             <p>- Більше 10-ти мов програмування</p>
                             <p>та сучасних фреймворків</p> 
@@ -150,7 +150,7 @@ const Learn = (props) => {
                             <p>- Ораторське мистецтво</p>
                         </div>
                     </div>
-                    <div className='d-a-t col-3'>
+                    <div className='d-a-t-learn col-3'>
                         <div className='img-a'>
                             <span>
                                 <h4>ДУХОВНИЙ</h4>
@@ -158,7 +158,7 @@ const Learn = (props) => {
                                 <img src={Krest} alt=''/>
                             </span>
                         </div>
-                        <div className='d-a-p'>
+                        <div className='d-a-p-learn'>
                             <p>- Біблійні читання </p>
                             <p>- Духовне спілкування з наставником</p>
                             <p>- Участь в таборах</p>
@@ -167,7 +167,7 @@ const Learn = (props) => {
                             <p>та на львівських подіях.</p>
                         </div>
                     </div>
-                    <div className='d-a-t col-3'>
+                    <div className='d-a-t-learn col-3'>
                         <div className='img-a'>
                             <span className=''>
                               <h4>ФІЗИЧНИЙ</h4>
@@ -175,7 +175,7 @@ const Learn = (props) => {
                               <img src={Sport} alt=''/>
                             </span>
                         </div>
-                        <div className='d-a-p'>
+                        <div className='d-a-p-learn'>
                             <p>- Легка атлетика</p>
                             <p>- Футбол</p>
                             <p>- Абонемент в Sport Life</p> 
@@ -185,11 +185,11 @@ const Learn = (props) => {
                 
             </div>
 
-            <div className='success'>
-                <div className='s-h'>
+            <div className='success-learn'>
+                <div className='s-h-learn'>
                     <h1>Історії успіху наших випускників</h1>
                 </div>
-                <div className='s-info'>
+                <div className='s-info-learn'>
                        {ArrSuccess.map( Success => {
                          return(  
                              <div className="col-4">
