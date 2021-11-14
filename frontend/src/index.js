2e59235f5838097ecf7f3212a72a297bba4c4b79
AlexStasiuk <sov853972@gmail.com>
2021-11-14 19:51:26 +0200

    change project structure

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/app';
+import App from './components/desktop/app';
 
+const isMobile = window.innerWidth <= 500;
+if(!isMobile){
 
-ReactDOM.render(
+  ReactDOM.render(
 
-<App/>,
-  document.getElementById('root')
-);
+  <App/>,
+    document.getElementById('root')
+  );
+}
+else{
+  ReactDOM.render(
+
+    <h1>mobileTest</h1>,
+      document.getElementById('root')
+    );
+}
